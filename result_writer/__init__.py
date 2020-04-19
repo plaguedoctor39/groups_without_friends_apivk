4328935c0cee58be76298bae8c8f0b194ed24e35
daniil <shishovdanil@gmail.com>
2020-04-19 13:08:44 +0300

    moved file writer

---
+++ result_writer/__init__.py
@@ -0,0 +1,5 @@
+import json
+def file_writer(json_):
+    with open('groups.json', 'w', encoding='utf8') as f:
+        f.write(json.dumps(json_, ensure_ascii=False))  # to json file
+        print('Информация успешно загруженна')
